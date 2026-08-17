a8b35f354faa0772e966932e77277ed5fae55bf7
Higor <pessoaizaac@gmail.com>
2026-08-17 17:21:39 -0300

    Iteradores

--- Estudos/Iteradores.py
+++ Estudos/Iteradores.py
@@ -17,10 +17,17 @@ for n in lista:
 # Exercitando
 
 numeros = [10, 20, 30, 40, 50]
-print(hasattr(numeros, '__iter__'))
+# print(hasattr(numeros, '__iter__'))  # Retorna True (é iterável)
+# print(hasattr(numeros, '__next__'))  # Retorna False (não é um iterador ainda)
 
-print(next(numeros))
-print(next(numeros))
-print(next(numeros))
-print(next(numeros))
-print(next(numeros))
+# CORREÇÃO: Transforma a lista em iterador
+numeros = iter(numeros) 
+
+# print(hasattr(numeros, '__next__'))  # Agora retorna True!
+
+# Agora o next() vai funcionar perfeitamente:
+print(next(numeros))  # Imprime 10
+print(next(numeros))  # Imprime 20
+print(next(numeros))  # Imprime 30
+print(next(numeros))  # Imprime 40
+print(next(numeros))  # Imprime 50
