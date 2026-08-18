f3a2036004c3d2c54ef184be1b8f9542de5f395c
Higor <pessoaizaac@gmail.com>
2026-08-18 14:32:34 -0300

    Decorators ( Args / Kwargs )

--- Estudos/Decorators.py
+++ Estudos/Decorators.py
@@ -19,3 +19,31 @@ calcular_potencia(2,2)
 
 
 
+# Args e Kwargs
+
+# *args --> permite você pasar quantos valores de posição você quiser
+
+def calcular_imposto(valor, *pecr_ir):
+    total_imposto = 0
+    for v in pecr_ir:
+        total_imposto += valor * v
+    return total_imposto
+
+
+print(calcular_imposto(1000, 0.275, 0.05, 0.0375, 0.03))
+
+# **kwargs é o args, porém você deve nomear os parâmetros!
+# kwargs.value() --> para você acessar os valores dados para os parâmetros
+
+def cll_imps(valor, **kwargs):
+    total_imposto = 0
+    for v in kwargs.values():
+        total_imposto += valor * v
+    return total_imposto
+
+print(cll_imps(1000, perc_ir=0.275, perc_iss=0.05, perc_csll=0.0375, perc_pis=0.03))
+
+
+
+
+
